80cea392cb115db8f172a9768a6cdbd1a401d020
XinLang2019 <1903152960@qq.com>
2025-10-17 14:27:03 +0800

    add go2 robot

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -34,6 +34,7 @@ from .base.legged_robot import LeggedRobot
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .a1.a1_amp_config import A1AMPCfg, A1AMPCfgPPO
 
+from .go2.go2_amp_config import Go2AMPCfg, Go2AMPCfgPPO
 
 import os
 
@@ -41,3 +42,5 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "a1_amp", LeggedRobot, A1AMPCfg(), A1AMPCfgPPO() )
+
+task_registry.register( "go2_amp", LeggedRobot, Go2AMPCfg(), Go2AMPCfgPPO() )
